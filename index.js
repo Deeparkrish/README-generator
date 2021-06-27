3ed9a00b7588794905644938a31c64ec74249929
Deepa <kosalai17@gmail.com>
2021-06-27 01:17:24 -0700

    got project information from commandline

--- index.js
+++ index.js
@@ -1,13 +1,174 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const { resolve } = require("path");
 
+const generateReadMe = require("./utils/generateMarkdown")
+ const readMeFile ="./dis/index.html";
 // TODO: Create an array of questions for user input
-const questions = [];
+const answers = [];
+const promptUser = () => {
+
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'projectTitle',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter your project title!');
+            return false;
+          }
+        },
+        message: 'What is your project title?'
+      },
+      {
+        type: 'input',
+        name: 'projectDescription',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a brief description about your project!');
+            return false;
+          }
+        },
+        message: 'Enter a brief description of the project'
+      },
+      
+      {
+        type: 'input',
+        name: 'projectUsage',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please provide instructions and examples for use.');
+            return false;
+          }
+        },
+        message: 'Please provide instructions and examples for use.'
+      },
+      {
+        type: "list",
+        name: "projectlicense",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open",
+            "Boost"
+        ]
+      },
+      {
+        type: 'confirm',
+        name: 'confirmContribute',
+        message: 'Would you  like add contributors to this project?',
+        default: true
+      },
+      {
+        type: "input",
+        name: "projectContribution",
+        message: 'Provide some information about yourself:',
+        when: ({ confirmContribute }) => confirmContribute,
+        validate: projectContribution => {
+            if (projectContribution) {
+              return true;
+            } else {
+              console.log('Please provide contribution guidelines');
+              return false;
+            }
+          },
+        
+        message: "Please  specify the Contribution guidlines for your project."
+    },
+    {
+        type: "input",
+        name: "projectTesting",
+        validate: testInput => {
+            if (testInput) {
+              return true;
+            } else {
+              console.log('Please provide test instructions.');
+              return false;
+            }
+          },
+        message: "Please provide  information about test cases and how to run them "
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub Username (Required)",
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address(Required)",
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your e-mail address!');
+            return false;
+          }
+        }
+      },
+
+    ])
+    .then(answers =>{    
+        return answers;
+    });
+  };
+
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile=(fileName, data) => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile(fileName,data,err=>{
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+
+        });
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    .then(answers =>{
+        return generateReadMe(answers);
+    })
+    .then(pageHTMl => {
+        return writeToFile(readMeFile,pageHTMl);
+    })
+    .catch(err =>{
+        console.log (err);
+    });
+
+}
 
 // Function call to initialize app
 init();
